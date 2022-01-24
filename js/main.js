92471c35322e1e5c45071472ae2ac57364ef050e
P1vko <60178690+P1vko@users.noreply.github.com>
2022-01-24 19:24:38 +0300

    partners upload

--- js/main.js
+++ js/main.js
@@ -16,3 +16,34 @@ document.addEventListener( 'DOMContentLoaded', function() {
     } );
     splide.mount();
   } );
+
+  document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.partners-slider_ltr', {
+        pagination: false,
+        arrows: false,
+        type: 'loop',
+        direction: 'rtl',
+        autoWidth: true,
+        fixedHeight: '61px',
+        autoplay: true,
+        interval: 1,
+        speed: 1200,
+        pauseOnHover: true,
+    } );
+    splide.mount();
+  } );
+  document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.partners-slider_rtl', {
+        pagination: false,
+        arrows: false,
+        type: 'loop',
+        direction: 'ltr',
+        autoWidth: true,
+        fixedHeight: '61px',
+        autoplay: true,
+        interval: 1,
+        speed: 1200,
+        pauseOnHover: true,
+    } );
+    splide.mount();
+  } );
